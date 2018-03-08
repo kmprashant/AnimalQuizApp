3d62780213263c3ac35b6ffb46bafac2b64f6855
kmprashant <prashant207kumar@gmail.com>
2018-03-09 03:56:03 +0530

    Addig Complete File

---
+++ app/src/main/java/com/example/android/animalquiz/SettingsActivityFragment.java
@@ -0,0 +1,23 @@
+package com.example.android.animalquiz;
+
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SettingsActivityFragment extends PreferenceFragment {
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    addPreferencesFromResource(R.xml.quiz_preferences);
+    }
+}
